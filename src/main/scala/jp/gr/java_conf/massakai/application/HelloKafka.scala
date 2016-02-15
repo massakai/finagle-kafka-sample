2a2e8380e2d10659b38f988f6666d81ceb29e165
Masashi SAKAI <masashi.sakai1986@gmail.com>
2016-02-16 00:38:02 +0900

    read kafka settings from json

--- src/main/scala/jp/gr/java_conf/massakai/application/HelloKafka.scala
+++ src/main/scala/jp/gr/java_conf/massakai/application/HelloKafka.scala
@@ -1,37 +1,46 @@
 package jp.gr.java_conf.massakai.application
 
-import kafka.api.FetchRequestBuilder
-import kafka.javaapi.consumer.SimpleConsumer;
+import jp.gr.java_conf.massakai.kafka.{Config, KafkaConsumer}
+import scala.io.Source
+import org.json4s.native.JsonMethods._
 
 object HelloKafka extends App {
-  val leadBroker = "localhost"
-  val port = 9093
-  val clientName = "HelloKafka"
-  val consumer = new SimpleConsumer(leadBroker, port, 100000, 64 * 1024, clientName)
+  // FIXME: 設定ファイルのパスは実行引数から取得する
+  val configPath = "src/main/resources/config.json"
+  val configSource = Source.fromFile(configPath)
+  val configJson = parse(configSource mkString)
 
-  val topic: String = "test"
-  val partition = 0
-  var offset: Long = 0
-  val request = new FetchRequestBuilder()
-    .clientId(clientName)
-    .addFetch(topic, partition, offset, 100000) // Note: this fetchSize of 100000 might need to be increased if large batches are written to Kafka
-    .build()
+  implicit val formats = org.json4s.DefaultFormats
+  val config = configJson.extract[Config]
+  val broker = config.bootstrap.head
+  // FIXME: パーティション毎にリーダーを選択する
+  val clientName = "HelloKafka_" + broker.host + "_" + broker.port
+  val consumer = new KafkaConsumer(
+    broker.host,
+    broker.port,
+    config.consumer.soTimeout,
+    config.consumer.bufferSize,
+    clientName)
 
-  val response = consumer.fetch(request)
+  val topic = config.topic.head
+  val topicName = topic.name
+  val partition = topic.partition.head
+  val partitionId = partition.id
+  var offset = partition.offset
+  val response = consumer.getMessages(topicName, partitionId, offset, config.consumer.fetchSize)
   if (response.hasError) {
-
+    // TODO: エラー処理を追加する
+    println("Error")
   } else {
-    val messageAndOffsetIterator = response.messageSet(topic, partition).iterator()
+    val messageAndOffsetIterator = response.messageSet(topicName, partitionId).iterator()
     while (messageAndOffsetIterator.hasNext) {
       val messageAndOffset = messageAndOffsetIterator.next()
       val payload = messageAndOffset.message.payload
       val bytes = new Array[Byte](payload.limit)
       messageAndOffset.message.payload.get(bytes)
-      val message = new String(bytes, "UTF-8")
+      val message = new String(bytes)
       println(messageAndOffset.offset + ": " + message)
       offset = messageAndOffset.nextOffset
     }
   }
-
-  consumer.close()
 }
